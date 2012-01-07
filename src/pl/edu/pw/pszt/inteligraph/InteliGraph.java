eb0fa109961a069de399585bd2b5336b55919779
Wojtek <wbkaczorowski@gmail.com>
2012-01-07 17:54:36 +0100

    nic

--- src/pl/edu/pw/pszt/inteligraph/InteliGraph.java
+++ src/pl/edu/pw/pszt/inteligraph/InteliGraph.java
@@ -5,19 +5,18 @@ import pl.edu.pw.pszt.inteligraph.events.EventsBlockingQueue;
 import pl.edu.pw.pszt.inteligraph.model.Model;
 import pl.edu.pw.pszt.inteligraph.view.View;
 
-
 public class InteliGraph {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-    	EventsBlockingQueue blockingQueue = new EventsBlockingQueue();
-    	Model model = new Model();
-    	View view = new View(blockingQueue);
-    	Controller controller = new Controller(model, view, blockingQueue);
-    	
-    	controller.programStart();
+	EventsBlockingQueue blockingQueue = new EventsBlockingQueue();
+	Model model = new Model();
+	View view = new View(blockingQueue);
+	Controller controller = new Controller(model, view, blockingQueue);
+
+	controller.programStart();
     }
 
 }
