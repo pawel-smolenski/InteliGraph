4920be2e22274a8e33375f69c723931363b7438c
Paweł Smoleński <P.Smolenski@stud.elka.pw.edu.pl>
2012-01-07 12:51:05 +0100

    Świeży projekt

---
+++ src/pl/edu/pw/pszt/inteligraph/InteliGraph.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.pszt.inteligraph;
+
+public class InteliGraph {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
