9c8d2c17ea30ef030a1287f368158d9f88739274
Wojtek <wbkaczorowski@gmail.com>
2012-01-07 14:14:20 +0100

    podstawowe GUI dodane

---
+++ src/pl/edu/pw/pszt/inteligraph/Constans.java
@@ -0,0 +1,5 @@
+package pl.edu.pw.pszt.inteligraph;
+
+public class Constans {
+    public static String APP_NAME = "InteliGraph";
+}
---
+++ src/pl/edu/pw/pszt/inteligraph/GUI/InteliGraph.java
@@ -0,0 +1,67 @@
+package pl.edu.pw.pszt.inteligraph.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import pl.edu.pw.pszt.inteligraph.Constans;
+
+public class InteliGraph {
+    private JFrame f = new JFrame(Constans.APP_NAME);
+    // Menu
+    private JMenuBar mb = new JMenuBar(); 
+    private JMenu mnuFile = new JMenu("File"); 
+    private JMenuItem mnuItemQuit = new JMenuItem("Quit");
+    private JMenu mnuHelp = new JMenu("Help");
+    private JMenuItem mnuItemAbout = new JMenuItem("About");
+
+    /** 
+     * Konstruktor GUI
+     */
+    public InteliGraph() {
+	
+	f.setJMenuBar(mb);
+
+
+	mnuFile.add(mnuItemQuit); 
+	mnuHelp.add(mnuItemAbout); 
+	mb.add(mnuFile); 
+	mb.add(mnuHelp);
+
+	f.getContentPane().setLayout(new BorderLayout());
+	f.addWindowListener(new ListenCloseWdw());
+	mnuItemQuit.addActionListener(new ListenMenuQuit());
+    }
+
+    public class ListenMenuQuit implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    System.exit(0);
+	}
+    }
+
+    public class ListenCloseWdw extends WindowAdapter {
+	public void windowClosing(WindowEvent e) {
+	    System.exit(0);
+	}
+    }
+
+    public void launchFrame() {
+	// Display Frame
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	f.pack();
+	
+	f.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+	InteliGraph gui = new InteliGraph();
+	gui.launchFrame();
+    }
+}
