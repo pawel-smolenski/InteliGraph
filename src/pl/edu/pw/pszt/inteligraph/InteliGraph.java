9a35e4e25b6b53267be87d4643e454e41fe98137
Paweł Smoleński <P.Smolenski@stud.elka.pw.edu.pl>
2012-01-07 17:54:06 +0100

    Działający przykład użycia JUNG-a

--- src/pl/edu/pw/pszt/inteligraph/InteliGraph.java
+++ src/pl/edu/pw/pszt/inteligraph/InteliGraph.java
@@ -4,6 +4,9 @@ import pl.edu.pw.pszt.inteligraph.controller.Controller;
 import pl.edu.pw.pszt.inteligraph.events.EventsBlockingQueue;
 import pl.edu.pw.pszt.inteligraph.model.Model;
 import pl.edu.pw.pszt.inteligraph.view.View;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.graph.util.EdgeType;
 
 
 public class InteliGraph {
@@ -17,7 +20,34 @@ public class InteliGraph {
     	View view = new View(blockingQueue);
     	Controller controller = new Controller(model, view, blockingQueue);
     	
-    	controller.programStart();
+//    	controller.programStart();
+    	SimpleGraph simpleGraph = new SimpleGraph();
+    }
+    
+    public void jung()
+    {
+    	 Graph<Integer, String> g = new SparseMultigraph<Integer, String>();
+    			 
+    	 g.addVertex((Integer)1);
+         g.addVertex((Integer)2);
+         g.addVertex((Integer)3); 
+         // Add some edges. From above we defined these to be of type String
+         // Note that the default is for undirected edges.
+         g.addEdge("Edge-A", 1, 2); // Note that Java 1.5 auto-boxes primitives
+         g.addEdge("Edge-B", 2, 3);       
+         // Let's see what we have. Note the nice output from the
+         // SparseMultigraph<V,E> toString() method
+         System.out.println("The graph g = " + g.toString());
+         // Note that we can use the same nodes and edges in two different graphs.
+         Graph<Integer, String> g2 = new SparseMultigraph<Integer, String>();
+         g2.addVertex((Integer)1);
+         g2.addVertex((Integer)2);
+         g2.addVertex((Integer)3); 
+         g2.addEdge("Edge-A", 1,3);
+         g2.addEdge("Edge-B", 2,3, EdgeType.DIRECTED);
+         g2.addEdge("Edge-C", 3, 2, EdgeType.DIRECTED);
+         g2.addEdge("Edge-P", 2,3); // A parallel edge
+         System.out.println("The graph g2 = " + g2.toString());  
     }
 
 }
---
+++ src/pl/edu/pw/pszt/inteligraph/SimpleGraph.java
@@ -0,0 +1,38 @@
+package pl.edu.pw.pszt.inteligraph;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseGraph;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+ 
+public class SimpleGraph extends JFrame {
+  public SimpleGraph (){
+    super("Mój pierwszy graf");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    Graph g = getGraph();
+    VisualizationViewer<Integer,String> vv = 
+     new VisualizationViewer<Integer,String>(new FRLayout(g),
+     new Dimension (300,200));
+    getContentPane().add(vv);
+ 
+    pack();
+    setSize (new Dimension (400, 300));
+    setVisible(true);
+  }
+ 
+  public Graph getGraph() {
+    Graph<Integer, String> g = new SparseGraph<Integer, String>();
+    g.addVertex((Integer)1);
+    g.addVertex((Integer)2);
+    g.addVertex((Integer)3);
+    g.addEdge("Edge-A", 1, 2);
+    g.addEdge("Edge-B", 2, 3);
+    g.addEdge("Edge-C", 3, 1);
+    return g;
+  }
+}
