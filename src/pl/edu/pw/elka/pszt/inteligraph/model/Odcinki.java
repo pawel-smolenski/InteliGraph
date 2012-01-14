6a68c2a295954e546c800bf16adb5817538a4547
Piotrek <sokol121@gmail.com>
2012-01-14 19:31:36 +0100

    naprawa

--- src/pl/edu/pw/elka/pszt/inteligraph/model/Odcinki.java
+++ src/pl/edu/pw/elka/pszt/inteligraph/model/Odcinki.java
@@ -11,14 +11,16 @@ private static int przynaleznosc(int xx, int xy, int yx, int yy, int zx, int zy)
 		int det; //wyznacznik macierzy
 		 
 		det = xx*yy + yx*zy + zx*xy - zx*yy - xx*zy - yx*xy;
+		
 		if (det!=0)
 		return 0 ;
-		else {
-		if ((Math.min(xx, yx) <= zx) && (zx <= Math.max(xx, yx)) &&
-		(Math.min(xy, yy) <= zy) && (zy <= Math.max(xy, yy)))
-		return 1;
-		else
-		return 0;
+		else 
+		{
+			if ((Math.min(xx, yx) <= zx) && (zx <= Math.max(xx, yx)) &&
+			(Math.min(xy, yy) <= zy) && (zy <= Math.max(xy, yy)))
+			return 1;
+			else
+			return 0;
 		}
 	}
 	 
@@ -28,13 +30,14 @@ private static int det_matrix(int xx, int xy, int yx, int yy, int zx, int zy)
 	return (xx*yy + yx*zy + zx*xy - zx*yy - xx*zy - yx*xy);
 	}
 	 
-public  void main(Point beg_1, Point end_1, Point beg_2, Point end_2) 
+//Point beg_1, Point end_1, Point beg_2, Point end_2
+public  void przecinanie() 
 	{
 		//int i,det; //wyznacznik macierzy
 		int x[] = new int[4]; //tablica wspolrzednych x punktow
 		int y[] = new int[4]; //tablica wspolrzednych y punktow
 
-		x[0] =  beg_1.x;
+		/*x[0] =  beg_1.x;
 		y[0] =  beg_1.y;
 		
 		x[1] = end_1.x;
@@ -44,7 +47,18 @@ public  void main(Point beg_1, Point end_1, Point beg_2, Point end_2)
 		y[2] = beg_2.y;
 		
 		x[3] = end_2.x;
-		y[3] = end_2.y;
+		y[3] = end_2.y;*/
+		x[0] =  1;
+		y[0] =  1;
+		
+		x[1] = 1;
+		y[1] = 3;
+		
+		x[2] = 2;
+		y[2] = 3;
+		
+		x[3] = 2;
+		y[3] = 0;
 		 
 		//      Sprawdzanie, czy jakiś punkt należy do drugiego odcinka
 		if (przynaleznosc(x[0], y[0], x[1], y[1], x[2], y[2])==1) System.out.println("Odcinki sie przecinaja- przynaleznosc"); else
